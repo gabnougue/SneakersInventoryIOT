8c829f5087dce336fac1486b7eb36cc7757e160c
Alex <alexandreduro11@gmail.com>
2024-01-26 15:05:04 +0100

    Changes and added main.py

--- consumers/sneaker_consumer.py
+++ consumers/sneaker_consumer.py
@@ -5,19 +5,19 @@ def callback(ch, method, properties, body):
     print(f" [x] Received {body}")
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
+def run():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+    channel = connection.channel()
 
-channel.queue_declare(queue='sneakers_queue')
+    channel.queue_declare(queue='sneakers_queue')
 
-channel.basic_consume(queue='sneakers_queue',
-                      on_message_callback=callback,
-                      auto_ack=True)
+    channel.basic_consume(queue='sneakers_queue',
+                          on_message_callback=callback,
+                          auto_ack=True)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
-
-try:
+    print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
-except KeyboardInterrupt:
-    channel.stop_consuming()
-connection.close()
+
+
+if __name__ == "__main__":
+    run()
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# main.py
+import threading
+from producers import sneaker_producer
+from consumers import sneaker_consumer
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def start_producer():
+    sneaker_producer.run()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def start_consumer():
+    sneaker_consumer.run()
+
+
+if __name__ == "__main__":
+    producer_thread = threading.Thread(target=start_producer)
+    producer_thread.start()
+
+    consumer_thread = threading.Thread(target=start_consumer)
+    consumer_thread.start()
+
+    producer_thread.join()
+    consumer_thread.join()
